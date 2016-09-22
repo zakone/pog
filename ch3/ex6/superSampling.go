6e9e5f9c9e2f896640baee9b15a705cc4c40d1ee
zakone <yohi0418@gmail.com>
2016-09-23 01:57:46 +0900

    update ch3&4

--- ch3/ex6/superSampling.go
+++ ch3/ex6/superSampling.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "github.com/nfnt/resize"
     "fmt"
     "image"
     "image/png"
@@ -30,16 +31,22 @@ func superSampling(img image.Image) image.Image {
         complex(1, 0),
         complex(1, 1),
     }
-
-    superImg := image.NewRGBA(image.Rect(0, 0, img.Width*2, img.Height*2))
-    for i := 0; i < img.Width; i++ {
-        for j := 0; j < img.Height; j++ {
+    b := img.Bounds()
+    if b.Empty() {
+        return nil
+    }
+    scaledImg := image.NewRGBA(image.Rect(0, 0, b.Dx()*2, b.Dy()*2))
+    for i := 0; i < b.Dx(); i++ {
+        for j := 0; j < b.Dy(); j++ {
             pointColor := img.At(i, j)
             k := complex(float64(i*2), float64(j*2))
-            for subP := range subPoints {
-                superImg.Set(real(k+subP), imag(k+subP), pointColor)
+            for _, subP := range subPoints {
+                scaledImg.Set(int(real(k+subP)), int(imag(k+subP)), pointColor)
             }
         }
     }
-    return superImg
+    sampleImg := resize.Resize(uint(b.Dx()), uint(b.Dy()), scaledImg, resize.Lanczos3)
+    //sampleImg := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
+    //draw.Draw(sampleImg, sampleImg.Bounds(), scaledImg, image.Point{0,0}, draw.Src)
+    return sampleImg
 }
