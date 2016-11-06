6ddeed9a4a6bc548951e58ae1fb7c7844b514efb
YangFei <yohi0418@gmail.com>
2016-11-06 17:16:32 +0900

    add ch4/ex14

--- ch4/ex14/issueServer.go
+++ ch4/ex14/issueServer.go
@@ -1,42 +1,55 @@
+//go run issueServer.go &
+//localhost:8000/?repo=golang/go
 package main
 
 import (
 	"./github"
+	"fmt"
 	"html/template"
 	"log"
-	"os"
-	"time"
+	"net/http"
 )
 
-const templ = `{{.TotalCount}} issues:
-{{range .Items}}-------------------------------------
-Number: {{.Number}}
-User:   {{.User.Login}}
-Title:  {{.Title | printf "%.64s"}}
-Age:    {{.CreatedAt | daysAgo}} days
-{{end}}`
+const templ = `<h1>{{.TotalCount}} issues:</h1>
+<table>
+<tr style='text-align: left'>
+ <th>#</th>
+ <th>State</th>
+ <th>User</th>
+ <th>Title</th>
+</tr>
+{{range .Items}}
+<tr>
+    <td><a href='{{.HTMLURL}}'>{{.Number}}</a></td>
+    <td>{{.State}}</td>
+    <td><a href="{{.User.HTMLURL}}">{{.User.Login}}</a></td>
+    <td><a href="{{.HTMLURL}}">{{.Title}}</a></td>
+</tr>
+{{end}}
+</table>`
 
-func daysAgo(t time.Time) int {
-	return int(time.Since(t).Hours() / 24)
-}
-
-var issueList = template.Must(template.New("issuelist").
-	Funcs(template.FuncMap{"daysAgo": daysAgo}).
-	Parse(`
-		<h1>{{.TotalCount}} issues:</h1>
-{{range .Items}}-------------------------------------
-Number: {{.Number}}
-User:   {{.User.Login}}
-Title:  {{.Title | printf "%.64s"}}
-Age:    {{.CreatedAt | daysAgo}} days
-{{end}}`))
+var issueList = template.Must(template.New("issuelist").Parse(templ))
 
 func main() {
-	result, err := github.SearchIssues(os.Args[1:])
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		issueReport(w, r)
+	})
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func issueReport(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	var input []string
+	for k, v := range r.Form {
+		input = append(input, fmt.Sprintf("%s:%v"), k, v[0])
+	}
+	result, err := github.SearchIssues(input)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := report.Execute(os.Stdout, result); err != nil {
+	if err := issueList.Execute(w, result); err != nil {
 		log.Fatal(err)
 	}
 }
